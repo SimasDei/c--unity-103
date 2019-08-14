0372b3b394d1932bd9b5b1d03f8e68bab326021b
Simas <simonas.deimantavicius@mediapark.com>
2019-08-14 13:44:31 +0300

    Add initial state text

--- Assets/AdventureGame.cs
+++ Assets/AdventureGame.cs
@@ -11,7 +11,12 @@ public class AdventureGame : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        textComponent.text = "lorem ipsum";
+        textComponent.text = "Lorem ipsum dolor sit amet," +
+            " consectetur adipiscing elit. Morbi ultricies tortor sodales gravida luctus." +
+            " Mauris nec tristique nibh, aliquam dignissim orci. Morbi ac luctus tellus." +
+            " Nunc eget purus auctor elit euismod pharetra. Mauris aliquet tortor ut ipsum consequat," +
+            " non laoreet leo faucibus. Fusce ultricies, velit ac euismod scelerisque, ante turpis euismod dolor," +
+            " at facilisis ante leo in lectus. Curabitur ac est porttitor, ultricies mi et, malesuada risus.";
     }
 
     // Update is called once per frame
