5b3171aeb988477430a4530ee1d3e14c8fefb661
Simas <simonas.deimantavicius@mediapark.com>
2019-08-14 14:08:07 +0300

    Add initial state and call first public method

--- Assets/AdventureGame.cs
+++ Assets/AdventureGame.cs
@@ -7,16 +7,15 @@ public class AdventureGame : MonoBehaviour
 {
 
     [SerializeField] Text textComponent;
+    [SerializeField] State startingState;
+
+    State state;
 
     // Start is called before the first frame update
     void Start()
     {
-        textComponent.text = "Lorem ipsum dolor sit amet," +
-            " consectetur adipiscing elit. Morbi ultricies tortor sodales gravida luctus." +
-            " Mauris nec tristique nibh, aliquam dignissim orci. Morbi ac luctus tellus." +
-            " Nunc eget purus auctor elit euismod pharetra. Mauris aliquet tortor ut ipsum consequat," +
-            " non laoreet leo faucibus. Fusce ultricies, velit ac euismod scelerisque, ante turpis euismod dolor," +
-            " at facilisis ante leo in lectus. Curabitur ac est porttitor, ultricies mi et, malesuada risus.";
+        state = startingState;
+        textComponent.text = state.GetStateStory();
     }
 
     // Update is called once per frame
